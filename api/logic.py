c8048e32865585cb10c7fd16638955d70e70407b
SOHAMPATEL1483 <86504280+SOHAMPATEL1483@users.noreply.github.com>
2023-04-13 18:44:25 +0530

    chaned requirements

--- api/logic.py
+++ api/logic.py
@@ -1,5 +1,4 @@
 import numpy as np
-import tensorflow as tf
 import json
 from .models import Profile, Movies
 import pickle
@@ -14,7 +13,8 @@ def makeProfile(user):
     for i in range(10):
         lastrating.append(-1)
     userprofile = Profile.objects.create(
-        user=user, Lastrating=str(lastrating), R=str(r))
+        user=user, Lastrating=str(lastrating), R=str(r)
+    )
     userprofile.save()
 
 
@@ -25,7 +25,6 @@ def getFeature(user):
         userprofile = Profile.objects.get(user=user)
 
     except:
-
         makeProfile(user)
 
     return getMovieList(userprofile)
@@ -44,15 +43,15 @@ def getMovieList(userprofile):
             movie_ids.append(lr[i])
         if len(movie_ids) == 4:
             break
-    similarity = pickle.load(open('./static/similarity.pkl', 'rb'))
-    keyToimdb = pickle.load(open('./static/keyToImdb.pkl', 'rb'))
+    similarity = pickle.load(open("./static/similarity.pkl", "rb"))
+    keyToimdb = pickle.load(open("./static/keyToImdb.pkl", "rb"))
 
     movies = []
     for i in movie_ids:
         cnt = 5
         for j in similarity[i]:
             if j[0] not in r:
-                movies.append(keyToimdb[j[0]+1])
+                movies.append(keyToimdb[j[0] + 1])
                 cnt -= 1
             if cnt == 0:
                 break
@@ -65,10 +64,10 @@ def getMovieList(userprofile):
 def getPopularMovies(movies, r=[]):
     print("getPopularMovies successfully called")
     index = 0
-    popularity_list = pickle.load(open('./static/popularity_list.pkl', 'rb'))
-    imdbTokey = pickle.load(open('./static/imdbTokey.pkl', 'rb'))
+    popularity_list = pickle.load(open("./static/popularity_list.pkl", "rb"))
+    imdbTokey = pickle.load(open("./static/imdbTokey.pkl", "rb"))
     while len(movies) < 20 and index < 974:
-        if imdbTokey[popularity_list[index][0]]-1 not in r:
+        if imdbTokey[popularity_list[index][0]] - 1 not in r:
             movies.append(popularity_list[index][0])
         index += 1
     return movies
@@ -78,8 +77,11 @@ def getMovieDetails(movies):
     movie_list = []
     for i in movies:
         movie = Movies.objects.get(Imdb_id=i)
-        dicc = {'Imdb_id': movie.Imdb_id, 'Title': movie.Title,
-                'Poster_path': movie.Poster_path}
+        dicc = {
+            "Imdb_id": movie.Imdb_id,
+            "Title": movie.Title,
+            "Poster_path": movie.Poster_path,
+        }
         movie_list.append(dicc)
     return movie_list
     return movie_list
@@ -93,10 +95,10 @@ def updateRating(movieID, user, rating):
     for i in R:
         r.append(i[0])
     lr = json.loads(userprofile.Lastrating)
-    imdbTokey = pickle.load(open('./static/imdbTokey.pkl', 'rb'))
-    lr[rating-1] = imdbTokey[movieID]-1
-    if imdbTokey[movieID]-1 not in r:
-        R.append([imdbTokey[movieID]-1, rating])
+    imdbTokey = pickle.load(open("./static/imdbTokey.pkl", "rb"))
+    lr[rating - 1] = imdbTokey[movieID] - 1
+    if imdbTokey[movieID] - 1 not in r:
+        R.append([imdbTokey[movieID] - 1, rating])
 
     userprofile.R = str(R)
     userprofile.Lastrating = str(lr)
@@ -118,15 +120,15 @@ def getMovie(movieID, user):
     print("getMovie successfully called")
     movie = Movies.objects.filter(Imdb_id__contains=movieID).values()
     dictionary = {}
-    keyToImdb = pickle.load(open('./static/keyToImdb.pkl', 'rb'))
+    keyToImdb = pickle.load(open("./static/keyToImdb.pkl", "rb"))
     if len(movie) > 0:
-        dictionary['Movie'] = movie[0]
+        dictionary["Movie"] = movie[0]
     if user != None:
         userprofile = Profile.objects.get(user=user)
         R = json.loads(userprofile.R)
         for i in R:
-            if keyToImdb[i[0]+1] == movieID:
-                dictionary['Rating'] = i[1]
+            if keyToImdb[i[0] + 1] == movieID:
+                dictionary["Rating"] = i[1]
                 break
     return dictionary
 
@@ -135,7 +137,7 @@ def getUser(user):
     print("getUser successfully called")
     userprofile = Profile.objects.get(user=user)
     R = json.loads(userprofile.R)
-    keyToImdb = pickle.load(open('./static/keyToImdb.pkl', 'rb'))
+    keyToImdb = pickle.load(open("./static/keyToImdb.pkl", "rb"))
     movie_list = []
     for i in R:
         curr = {}
@@ -143,10 +145,10 @@ def getUser(user):
         val = i[0]
         val += 1
         imdb_id = keyToImdb[val]
-        curr['Imdb_id'] = imdb_id
+        curr["Imdb_id"] = imdb_id
         movie = Movies.objects.get(Imdb_id=imdb_id)
-        curr['Title'] = movie.Title
-        curr['Poste_path'] = movie.Poster_path
-        curr['Rating'] = i[1]
+        curr["Title"] = movie.Title
+        curr["Poste_path"] = movie.Poster_path
+        curr["Rating"] = i[1]
         movie_list.append(curr)
     return movie_list
